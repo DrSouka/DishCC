1128355c1cc5d32455f83ad5900b95f281437a10
Mindissu <36300672+Mindissu@users.noreply.github.com>
2021-03-12 11:30:51 +0100

    calories_calculator started

--- Réalisation/DishCC/view/calories_calculator.php
+++ Réalisation/DishCC/view/calories_calculator.php
@@ -1,14 +1,17 @@
-<div class='block list'>
-  <div class='element'>
-
-  </div>
+<div class='block ingredient' id='ingredient'>
+  <?php
+    require_once "model/dbManager.php";
+    $ingredientList = select(array('name', 'caloriesPer100g'), 'ingredient',);
+    foreach ($ingredientList as $key => $value) {
+      echo "<div class='element'>". $value['name'] ." ". $value['caloriesPer100g'] ."</div>";
+    }
+  ?>
+</div>
+<div class='block list' id='list'>
 </div>
 <div class='block'>
-  <div></div>
-  <div>How much calories?</div>
-  <div>
-    <div>Result</div>
-    <div>calories</div>
-  </div>
-  <div>Save in history</div>
+  <div class='add' onclick="document.getElementById('list').style.display = 'block';"></div>
+  <div class='element calories'>How much calories?</div>
+  <div class='element'><div class='value'>Result</div><p>calories</p></div>
+  <div class=' element save'>Save in history</div>
 </div>
